66d8a2a87a790c946d74cd25a06e35c657ec115a
Yousra <yousra.beddaou@gmail.com>
2021-08-03 12:15:06 +0100

    Create itemData.js

---
+++ itemData.js
@@ -0,0 +1,12 @@
+<List
+  itemData={data}
+  innerElementType="ul"
+  itemCount={data.length}
+  itemSize={20}
+  height={700}
+  width={400}
+>
+  {({ data, index, style }) => {
+    return <li style={style}>{data[index]}</li>;
+  }}
+</List>
